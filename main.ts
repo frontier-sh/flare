113ea4816f2abd81371bb42c266c81b1df7a550a
marcfowler <marc@frontier.sh>
2025-01-08 12:41:02 +1300

    Initial version

--- main.ts
+++ main.ts
@@ -1,134 +1,178 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, Setting, PluginSettingTab, App } from 'obsidian';
+import simpleGit, { SimpleGit } from 'simple-git';
 
-// Remember to rename these classes and interfaces!
+const WORKER_URL = 'https://flare.frontier.sh';
+const GITHUB_CLIENT_ID = 'Ov23liZYcWsgjBgJz5T5';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface FlareSettings {
+  githubToken: string;
+  repositoryOwner: string;
+  repositoryName: string;
+  state?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FlareSettings = {
+  githubToken: '',
+  repositoryOwner: '',
+  repositoryName: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FlarePlugin extends Plugin {
+  settings: FlareSettings;
+  private git: SimpleGit;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addCommand({
+      id: 'publish-post',
+      name: 'Publish Post',
+      callback: () => this.publishPost()
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.addSettingTab(new FlareSettingTab(this.app, this));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (this.settings.githubToken) {
+      await this.initGit();
+    }
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async initGit() {
+    this.git = simpleGit({
+      baseDir: (this.app.vault.adapter as any).getBasePath(),
+    });
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  async publishPost() {
+    if (!this.settings.githubToken) {
+      new Notice('Please connect to GitHub first in the Flare settings');
+      return;
+    }
 
-	onunload() {
+    try {
+      const activeFile = this.app.workspace.getActiveFile();
+      if (!activeFile) {
+        new Notice('No active file to publish');
+        return;
+      }
 
-	}
+      const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+      const branchName = `post-${timestamp}`;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      // Create and checkout new branch
+      await this.git.checkoutLocalBranch(branchName);
+      
+      // Stage and commit changes
+      await this.git.add(activeFile.path);
+      const commitMessage = `Add blog post: ${activeFile.basename}`;
+      await this.git.commit(commitMessage);
+      await this.git.push('origin', branchName);
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+      // Create PR using GitHub API
+      const prResponse = await fetch(
+        `https://api.github.com/repos/${this.settings.repositoryOwner}/${this.settings.repositoryName}/pulls`,
+        {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${this.settings.githubToken}`,
+            'Accept': 'application/vnd.github.v3+json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            title: commitMessage,
+            head: branchName,
+            base: 'main',
+            body: 'Created via Flare',
+            auto_merge: true
+          })
+        }
+      );
+
+      if (!prResponse.ok) {
+        throw new Error(`Failed to create PR: ${await prResponse.text()}`);
+      }
+
+      new Notice('Successfully created PR with auto-merge enabled');
+    } catch (error) {
+      new Notice(`Failed to publish: ${error.message}`);
+      console.error('Publishing error:', error);
+    }
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+class FlareSettingTab extends PluginSettingTab {
+	plugin: FlarePlugin;
+  
+	constructor(app: App, plugin: FlarePlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
+	  const { containerEl } = this;
+	  containerEl.empty();
+  
+	  if (!this.plugin.settings.githubToken) {
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		  .setName('Connect to GitHub')
+		  .setDesc('Authenticate with GitHub and select your repository')
+		  .addButton(button => button
+			.setButtonText('Connect')
+			.onClick(() => {
+			  const state = crypto.randomUUID();
+			  this.plugin.settings.state = state;
+			  this.plugin.saveSettings();
+  
+			  // Open web app directly for auth and repo selection
+			  window.open(`${WORKER_URL}/?client=obsidian&state=${state}`, '_blank');
+			  this.pollForSetup(state);
+			}));
+	  } else {
+		new Setting(containerEl)
+		  .setName('GitHub Status')
+		  .setDesc(`Connected to ${this.plugin.settings.repositoryOwner}/${this.plugin.settings.repositoryName}`)
+		  .addButton(button => button
+			.setButtonText('Disconnect')
+			.onClick(async () => {
+			  this.plugin.settings.githubToken = '';
+			  this.plugin.settings.repositoryOwner = '';
+			  this.plugin.settings.repositoryName = '';
+			  await this.plugin.saveSettings();
+			  this.display();
+			}));
+	  }
 	}
-}
+  
+	async pollForSetup(state: string) {
+	  const checkSetup = async () => {
+		try {
+		  const response = await fetch(`${WORKER_URL}/api/check-setup?state=${state}`);
+		  if (response.ok) {
+			const data = await response.json();
+			this.plugin.settings.githubToken = data.access_token;
+			this.plugin.settings.repositoryOwner = data.owner;
+			this.plugin.settings.repositoryName = data.repo;
+			await this.plugin.saveSettings();
+			await this.plugin.initGit();
+			this.display();
+			new Notice('Successfully connected to GitHub!');
+			return;
+		  }
+		  setTimeout(checkSetup, 2000);
+		} catch (error) {
+		  console.error('Setup check failed:', error);
+		  setTimeout(checkSetup, 2000);
+		}
+	  };
+  
+	  checkSetup();
+	}
+  }
